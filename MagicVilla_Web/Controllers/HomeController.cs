08729f8fe6831368d0a363823f6cfb3023e9d1d9
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2023-01-30 23:49:27 +0300

    [dev]: images, bootstrap, black theme, home view implemented
    list villas in home view

--- MagicVilla_Web/Controllers/HomeController.cs
+++ MagicVilla_Web/Controllers/HomeController.cs
@@ -1,21 +1,36 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using MagicVilla_Web.Models;
+using AutoMapper;
+using MagicVilla_Web.Services.IServices;
+using Newtonsoft.Json;
+using MagicVilla_Web.Services;
+using MagicVilla_Web.Models.Dto;
 
 namespace MagicVilla_Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IVillaService _villaService;
+    private readonly IMapper _mapper;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IVillaService villaService, IMapper mapper)
     {
         _logger = logger;
+        _villaService = villaService;
+        _mapper = mapper;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        List<VillaDTO> list = new();
+        var response = await _villaService.GetAllAsync<APIResponse>();
+        if (response != null && response.IsSuccess)
+        {
+            list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+        }
+        return View(list);
     }
 
     public IActionResult Privacy()
