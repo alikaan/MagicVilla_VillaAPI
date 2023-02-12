97655d25cb6cedb4998a1112d29585c220694b91
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2023-02-13 00:55:35 +0300

    [dev]: IsUniqueUser, Register methods are implemented, for login secret key is added.

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -9,24 +9,49 @@ namespace MagicVilla_VillaAPI.Repository
 	public class UserRepository : IUserRepository
 	{
         private readonly ApplicationDbContext _db;
-		public UserRepository(ApplicationDbContext db)
+        private string secretKey;
+		public UserRepository(ApplicationDbContext db, IConfiguration configuration)
 		{
             _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
 		}
 
         public bool IsUniqueUser(string username)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(x => x.Username == username);
+            if(user == null)
+            {
+                return true;
+            }
+            return false;
         }
 
-        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        public async Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
         {
-            throw new NotImplementedException();
+            var user = _db.LocalUsers.FirstOrDefault(u => u.Username.ToLower() == loginRequestDTO.Username.ToLower()
+            && u.Password == loginRequestDTO.Password);
+            if(user == null)
+            {
+                return null;
+            }
+            // if user was found, generate JWT Token
+
         }
 
-        public Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
+        public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
         {
-            throw new NotImplementedException();
+            LocalUser user = new LocalUser()
+            {
+                Username = registrationRequestDTO.Username,
+                Password = registrationRequestDTO.Password,
+                Name = registrationRequestDTO.Name,
+                Role = registrationRequestDTO.Role,
+            };
+
+            _db.LocalUsers.Add(user);
+            await _db.SaveChangesAsync();
+            user.Password = "";
+            return user;
         }
     }
 }
