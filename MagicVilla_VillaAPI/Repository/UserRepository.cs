9e58fbc8e6ee3a59fbdb8e4cfc361beeafd6fd33
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2023-02-13 01:40:27 +0300

    [dev]: login method is implemented, related nuget packages are added.

--- MagicVilla_VillaAPI/Repository/UserRepository.cs
+++ MagicVilla_VillaAPI/Repository/UserRepository.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using System.Text;
 using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using MagicVilla_VillaAPI.Repository.IRepository;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 namespace MagicVilla_VillaAPI.Repository
 {
-	public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository
 	{
         private readonly ApplicationDbContext _db;
         private string secretKey;
@@ -35,7 +38,27 @@ namespace MagicVilla_VillaAPI.Repository
                 return null;
             }
             // if user was found, generate JWT Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var tokenDescripter = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
 
+            var token = tokenHandler.CreateToken(tokenDescripter);
+
+            LoginResponseDTO loginResponseDTO = new LoginResponseDTO()
+            {
+                Token = tokenHandler.WriteToken(token),
+                User = user
+            };
+            return loginResponseDTO;
         }
 
         public async Task<LocalUser> Register(RegistrationRequestDTO registrationRequestDTO)
