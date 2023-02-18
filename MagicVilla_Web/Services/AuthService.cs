501df03ef08700a53c7007eaeca4c4eee249181c
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2023-02-18 17:04:04 +0300

    [dev]: auth service is implemented to web project

---
+++ MagicVilla_Web/Services/AuthService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+
+namespace MagicVilla_Web.Services
+{
+	public class AuthService : BaseService, IAuthService
+	{
+        private readonly IHttpClientFactory _clientFactory;
+        private string _villaUrl;
+
+		public AuthService(IHttpClientFactory clientFactory, IConfiguration configuration) : base(clientFactory)
+		{
+            _clientFactory = clientFactory;
+            _villaUrl = configuration.GetValue<string>("ServiceUrls:VillaAPI");
+
+		}
+
+        public Task<T> LoginAsync<T>(LoginRequestDTO obj)
+        {
+            return SendAsync<T>(new Models.APIRequest()
+            {
+                ApiType = MagicVilla_Utility.SD.ApiType.POST,
+                Data = obj,
+                Url = _villaUrl + "/api/UsersAuth/login"
+            });
+        }
+
+        public Task<T> RegisterAsync<T>(UserDTO obj)
+        {
+            return SendAsync<T>(new Models.APIRequest()
+            {
+                ApiType = MagicVilla_Utility.SD.ApiType.POST,
+                Data = obj,
+                Url = _villaUrl + "/api/UsersAuth/register"
+            });
+        }
+    }
+}
+
