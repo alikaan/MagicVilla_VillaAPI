0652ce233d3aa0156bc3c28b23a6cb712eac8e98
Ali Kaan TURKMEN <turkmenalikaan@gmail.com>
2023-07-21 15:32:52 +0300

    [dev]: searching by villa name is implemented to code side not db

--- MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/v1/VillaAPIController.cs
@@ -35,7 +35,8 @@ namespace MagicVilla_VillaAPI.Controllers.v1
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy)
+        public async Task<ActionResult<APIResponse>> GetVillas([FromQuery(Name = "filterOccupancy")]int? occupancy,
+            [FromQuery] string? search)
 		{
             try
             {
@@ -47,6 +48,10 @@ namespace MagicVilla_VillaAPI.Controllers.v1
                 else
                 {
                     villaList = await _dbVilla.GetAllAsync();
+                }
+                if(!string.IsNullOrEmpty(search))
+                {
+                    villaList = villaList.Where(u => u.Name.ToLower().Contains(search));
                 }
                 _response.Result = _mapper.Map<List<VillaDTO>>(villaList);
                 _response.StatusCode = HttpStatusCode.OK;
